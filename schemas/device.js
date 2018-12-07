f7c3af8f180b12b787439a5f40a60a896c38c2f6
ryan-peirce <rjpeirce@oakland.edu>
2018-12-06 20:25:03 -0500

    Allow devices to have components updated

--- schemas/device.js
+++ schemas/device.js
@@ -21,6 +21,41 @@ var DeviceSchema = new mongoose.Schema({
   }]
 });
 
+//if id is from the same user update or add device
+DeviceSchema.statics.deviceCheckIn = function (deviceData) {
+  Device.findOne({ id: deviceData.id })
+    .exec(function (err, device) {
+      if (err) {
+        console.log(err);
+      } else if (!device) {
+        //use schema.create to insert device into the db
+        Device.create(deviceData, function (err, user) {
+          if (err) {
+            console.log("Error creating device: " + err);
+          } else {
+            console.log("device registered");
+          }
+        });
+      }
+      else{
+        if(deviceData.owner_email == device.owner_email){
+          Device.replaceOne({id: deviceData.id},deviceData, 
+            function(error, d) {
+              if(err){
+                console.log(err);
+              }
+              else{
+                console.log("Device updated");
+              }
+          });
+        }
+        else{
+          console.log("The device id is taken");
+        }
+      }
+   
+    });
+}
 
 var Device = mongoose.model('Device', DeviceSchema);
 module.exports = Device;
