bd19eeba6b433fa9f0638cc10bc8f4a4450f16e1
ryan-peirce <rjpeirce@oakland.edu>
2018-12-08 20:46:42 -0500

    Light component now updates on the page in real time.

---
+++ -D/light.js
@@ -0,0 +1,23 @@
+function pug_attr(t,e,n,f){return!1!==e&&null!=e&&(e||"class"!==t&&"style"!==t)?!0===e?" "+(f?t:t+'="'+t+'"'):("function"==typeof e.toJSON&&(e=e.toJSON()),"string"==typeof e||(e=JSON.stringify(e),n||-1===e.indexOf('"'))?(n&&(e=pug_escape(e))," "+t+'="'+e+'"'):" "+t+"='"+e.replace(/'/g,"&#39;")+"'"):""}
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function lightTemplate(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {var pug_debug_sources = {"views\u002Fclient\u002Flight.pug":"mixin light(comp)\r\n    if (comp.props.state == \"1\")\r\n        img(id=comp.id).onBulb(src=\"\u002Fviews\u002Fimages\u002FbulbOn1.png\" alt=\"Bulb On Image\")\r\n    else\r\n        img(id=comp.id).offBulb(src=\"\u002Fviews\u002Fimages\u002FbulbOff.png\" alt=\"Bulb Off Image\")\r\n    br\r\n    p #{comp.name}"};
+;pug_debug_line = 1;pug_debug_filename = "views\u002Fclient\u002Flight.pug";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line, pug_debug_sources[pug_debug_filename]);};return pug_html;}
